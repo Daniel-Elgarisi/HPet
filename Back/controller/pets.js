bdfa1400441172b10fa422651ab1bc8014fb5f8b
BeckyU96 <Beckyu96@gmail.com>
2021-12-25 15:36:39 +0200

    added back functions - Back

--- Back/controller/pets.js
+++ Back/controller/pets.js
@@ -1,34 +1,100 @@
-const pets = [
-    {type: "dog", breed: "shitzu", name: "ray", owner: "daniel"},
-    {type: "dog", breed: "sheperd", name: "belle", owner: "daniel"},
-    {type: "cat", breed: "siamese", name: "dani", owner: "daniel"},
-    {type: "cat", breed: "siamese", name: "philip", owner: "lahad"},
-]
-
+const { response } = require('express');
 const express = require('express');
+const client = require('../db/db');
 const router = express.Router();
 
-router.get('/pets', (req,res)=> {
+router.get('/pets', (req, res) => {
     let allpets = []
     for (let i = 0; i < pets.length; i++) {
-        if(pets[i].owner == req.body.owner){
-            if(pets[i].type==req.body.type){
+        if (pets[i].owner == req.body.owner) {
+            if (pets[i].type == req.body.type) {
                 allpets.push(pets[i])
             }
-        }  
+        }
     }
     res.status(200).contentType('application/json').json({
         "data": allpets
     })
 })
 
+router.post('/updateVaccein', (req, response) => {
+    client.query(`INSERT into vaccantions (vaccine_type,pet_id,date) 
+                VALUES ($1,$2,CURRENT_DATE)`,
+        [req.body.vaccein, req.body.petId],
+        (err, res) => {
+            if (err) {
+                console.log(err);
+                response.status(400).contentType('application/json').json({
+                    "message": "update failed!"
+                })
+            }
+            else {
+                response.status(200).contentType('application/json').json({
+                    "message": "update ok!"
+                })
+            }
 
-function CheckRole(Role)
-{
-    if(Role==1){
+        }
+    )
+})
+
+
+router.post('/findPetIdByName', (req, response) => {
+    client.query(`SELECT id FROM pets WHERE name=$1 AND owner_id=$2`, [req.body.pname, req.body.ownerId],
+        (err, res) => {
+            if (err) {
+                response.status(400).contentType('application/json').json({
+                    "message": "not found!"
+                })
+            }
+            else {
+                try {
+                    if (res.rows[0]) {
+                        console.log("owner: ", req.body.ownerId);
+                        console.log(res);
+                        console.log(res.rows[0]);
+                        response.status(200).contentType('application/json').json({
+                            "message": "ok!",
+                            "data": res.rows[0].id
+                        })
+                    }
+                }
+                catch {
+                    response.status(400).contentType('application/json').json({
+                        "message": "not found!"
+                    })
+                }
+
+            }
+        })
+})
+
+router.delete('/deletePet', (req, response) => {
+    client.query(`DELETE FROM pets WHERE name=$1 and owner_id=$2`, [req.body.pname, req.body.ownerId],
+        (err, res) => {
+            if (err) {
+                console.log(err);
+                response.status(400).contentType('application/json').json({
+                    "message": "delete failed!"
+                })
+            }
+            else {
+                console.log("owner: ", req.body.ownerId);
+                console.log(res);
+                response.status(200).contentType('application/json').json({
+                    "message": "delete ok!"
+                })
+            }
+
+        }
+    )
+})
+
+function CheckRole(Role) {
+    if (Role == 1) {
         return "OWNER"
     }
-    if(Role==2){
+    if (Role == 2) {
         return "SECRETARY"
     }
     return "DOCTOR"
--- Back/controller/user.js
+++ Back/controller/user.js
@@ -42,16 +42,74 @@ router.put('/update', (req, response) => {
     client.query('UPDATE users SET  first_name=$1, last_name=$2, email=$3, phone_number=$4 WHERE user_name = $5',
         [req.body.name, req.body.lname, req.body.email, req.body.phone, req.body.currentUser],
         (err, res) => {
-            if(err){
+            if (err) {
                 response.status(400).contentType('application/json').json({
                     "message": "edit failed!"
                 })
             }
-            response.status(200).contentType('application/json').json({
-                "message": "edit ok!"
-            })
+            else {
+                response.status(200).contentType('application/json').json({
+                    "message": "edit ok!"
+                })
+            }
+
         }
     )
 })
+
+router.post('/register', (req, response) => {
+    client.query(`INSERT into users (user_name, password, first_name, last_name, email, phone_number, role) 
+                VALUES($1, $2, $3, $4, $5, $6, $7)`,
+        [req.body.username, req.body.pass, req.body.name, req.body.lname, req.body.email, req.body.phone, req.body.role],
+        (err, res) => {
+            if (err) {
+                if (err.code == 23505) {
+                    console.log(err)
+                    response.status(400).contentType('application/json').json({
+                        "message": "username already exist!"
+                    })
+                }
+                else {
+                    console.log(err)
+                    response.status(400).contentType('application/json').json({
+                        "message": "register failed!"
+                    })
+                }
+            }
+            else {
+                response.status(200).contentType('application/json').json({
+                    "message": "register ok!"
+                })
+            }
+
+        }
+    )
+})
+
+router.post('/getUserByPhone', (req, response) => {
+    client.query(`SELECT id FROM users WHERE phone_number=$1`, [req.body.phone], (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        try {
+            if (res.rows[0]) {
+                console.log(res.rows[0].id);
+                response.status(200).contentType('application/json').json({
+                    "message": "ok!",
+                    "data": res.rows[0].id
+                })
+
+            }
+        }
+        catch(e){
+            console.log(e);
+            response.status(400).contentType('application/json').json({
+                "message": "not ok!"
+            })
+        }
+    })
+})
+
+
 
 module.exports = { router, varifyLogin } 
