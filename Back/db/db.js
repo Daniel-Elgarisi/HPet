2c56fc45f2de1f64b44e92aba0ea81d19dbbce06
BeckyU96 <Beckyu96@gmail.com>
2021-12-25 16:09:01 +0200

    updated all users menu nav & db pass

--- Back/db/db.js
+++ Back/db/db.js
@@ -3,8 +3,8 @@ const { Client } = require('pg')
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres',
-    password: '123',
+    database: 'Project',
+    password: '15975300asD',
     port: 5432,
 })
 
