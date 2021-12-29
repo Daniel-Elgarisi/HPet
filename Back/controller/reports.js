1721c5b72bd30d4846dadb02611c9a97651a0aa1
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2021-12-29 18:59:10 +0200

    Update reports.js
    make medical reports function

--- Back/controller/reports.js
+++ Back/controller/reports.js
@@ -5,3 +5,32 @@ const router = express.Router();
 router.post('/medical', MakeMedical);
 router.get('/:phonenumber/:petname', getAllPetReports)
 
+
+async function MakeMedical(req, response) {
+
+    let user = await client.query("select * from users where phone_number=$1", [req.body.phone_number]);
+    if (user.rowCount == 0) {
+        return response.status(400).json({ message: "user is not found" });
+    }
+
+    let userpets = await client.query('select * from pets where owner_id=$1 AND name=$2', [user.rows[0].id, req.body.name]);
+    if (userpets.rowCount == 0) {
+        return response.status(400).json({ message: "pet is not found" });
+    }
+
+    let sql = 'INSERT INTO medical_report(owner_id, pet_id, appoint_type, report, date)';
+    sql += " VALUES($1,$2,$3,$4, CURRENT_DATE)"
+    let vars = [user.rows[0].id, userpets.rows[0].id, req.body.appointment_type, req.body.report];
+
+
+    client.query(sql, vars, (err, res) => {
+        if (err) {
+            console.log(err);
+            return response.status(400).json({ message: "error" });
+        } else {
+            return response.status(200).json({ message: "report is created successfully" });
+        }
+    });
+
+
+}
