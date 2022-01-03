709449298da4803c6cb5f56c970c51c68c4519c4
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2022-01-03 20:50:00 +0200

    Update pets.js
    update pet status function

--- Back/controller/pets.js
+++ Back/controller/pets.js
@@ -111,6 +111,30 @@ async function addPet(req, response) {
 
 }
 
+async function UpdateStatusePet(req, response) {
+
+    let user = await client.query("select * from users where phone_number=$1", [req.params.phonenumber]);
+    if (user.rowCount == 0) {
+        return response.status(400).json({ message: "user is not found" });
+    }
+
+    let userpets = await client.query('select * from pets where owner_id=$1 AND name=$2', [user.rows[0].id, req.params.petname]);
+    if (userpets.rowCount == 0) {
+        return response.status(400).json({ message: "pet is not exist" });
+    }
+    let sql = "UPDATE pets SET status=$1 where owner_id=$2 AND name=$3";
+    let val = [false, user.rows[0].id, req.params.petname]
+    client.query(sql, val, (err, res) => {
+        if (err) {
+            response.status(400).json({ message: "Something went wrong" });
+        } else {
+            response.status(200).json({ message: "update successfully!" });
+        }
+    });
+
+
+}
+
 // router.get('/pets', (req, res) => {
 //     let allpets = []
 //     for (let i = 0; i < pets.length; i++) {
