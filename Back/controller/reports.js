2cf26c1f405df4ffde426705a323cd9a9bd3bbf2
EfratKadosh <95250966+EfratKadosh@users.noreply.github.com>
2022-01-04 17:16:00 +0200

    update getAllPetReports

--- Back/controller/reports.js
+++ Back/controller/reports.js
@@ -36,4 +36,38 @@ async function MakeMedical(req, response) {
 }
 
 
+
+async function getAllPetReports(req, response) {
+
+    let user = await client.query("select * from users where phone_number=$1", [req.params.phonenumber]);
+    if (user.rowCount == 0) {
+        return response.status(400).json({ message: "user is not found" });
+    }
+
+    let userpets = await client.query('select * from pets where owner_id=$1 AND name=$2', [user.rows[0].id, req.params.petname]);
+    if (userpets.rowCount == 0) {
+        return response.status(400).json({ message: "pet is not found" });
+    }
+
+    let sql = 'select * from medical_report where owner_id=$1 AND pet_id=$2';
+    let vars = [user.rows[0].id, userpets.rows[0].id];
+
+    client.query(sql, vars, (err, res) => {
+        if (err) {
+            console.log(err);
+            return response.status(400).json({ message: "error" });
+        } else {
+            let array = [];
+            res.rows.map((report) => {
+                let obj = { type: report.appoint_type, report: report.report }
+                array.push(obj)
+            })
+            return response.status(200).json(array);
+        }
+    });
+
+
+}
+
 module.exports = router;
+
