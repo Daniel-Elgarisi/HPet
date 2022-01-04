52e356d99e290592108305c2e4d2e7154fb1b585
EfratKadosh <95250966+EfratKadosh@users.noreply.github.com>
2022-01-04 16:25:00 +0200

    fix of make and delete

--- Back/controller/appointments.js
+++ Back/controller/appointments.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/make', MakeApp);
-router.delete('/', DeleteApp);
+router.delete('/delete', DeleteApp);
 router.get('/day', getAppOfDay);
 router.get('/month', getAppOfMonth);
 
 async function MakeApp(req, response) {
-
+    // let fixedTime =  new Date(req.body.time.toLocaleDateString())
     let user = await client.query("select * from users where phone_number=$1", [req.body.phone_number]);
     if (user.rowCount == 0) {
         return response.status(400).json({ message: "user is not found" });
@@ -19,21 +19,21 @@ async function MakeApp(req, response) {
         return response.status(400).json({ message: "pet is not found" });
     }
 
-    let ap = await client.query('select * from appointments where date=$1 AND time=$2', [req.body.date, req.body.time]);
+    let ap = await client.query('select * from appointments where time=$1', [req.body.time]);
     if (ap.rowCount > 0) {
         return response.status(400).json({ message: "Appointment is cell" });
     }
 
 
-    let sql = 'INSERT INTO appointments(appointment_type, date, time, owner_id, pet_id, phone_number)';
-    sql += " VALUES($1,$2,$3,$4,$5,$6)"
-    let vars = [req.body.appointment_type, req.body.date, req.body.time, user.rows[0].id, userpets.rows[0].id, req.body.phone_number];
+    let sql = 'INSERT INTO appointments(appointment_type, time, owner_id, pet_id, phone_number)';
+    sql += " VALUES($1,$2,$3,$4,$5)"
+    let vars = [req.body.appointment_type, req.body.time, user.rows[0].id, userpets.rows[0].id, req.body.phone_number];
 
 
     client.query(sql, vars, (err, res) => {
         if (err) {
             console.log(err);
-            return response.status(400).json({ message: "error" });   
+            return response.status(400).json({ message: "error" });
         } else {
             return response.status(200).json({ message: "appointments is created successfully" });
         }
@@ -42,7 +42,6 @@ async function MakeApp(req, response) {
 
 }
 
-//deleteapp
 async function DeleteApp(req, response) {
 
     let user = await client.query("select * from users where phone_number=$1", [req.body.phonenumber]);
@@ -55,13 +54,13 @@ async function DeleteApp(req, response) {
         return response.status(400).json({ message: "pet is not found" });
     }
 
-    let t = await client.query('select * from appointments where owner_id=$1 AND pet_id=$2 AND time=$3 AND date=$4', [user.rows[0].id, userpets.rows[0].id, req.body.time, req.body.date]);
-    if (userpets.rowCount == 0) {
+    let t = await client.query('select * from appointments where owner_id=$1 AND pet_id=$2 AND time=$3', [user.rows[0].id, userpets.rows[0].id, req.body.time]);
+    if (t.rowCount == 0) {
         return response.status(400).json({ message: "appointment is not found" });
     }
 
-    let sql = 'delete from appointments where owner_id=$1 AND pet_id=$2 AND time=$3 AND date=$4';
-    let vars = [user.rows[0].id, userpets.rows[0].id, t.rows[0].time, t.rows[0].date];
+    let sql = 'delete from appointments where owner_id=$1 AND pet_id=$2 AND time=$3';
+    let vars = [user.rows[0].id, userpets.rows[0].id, t.rows[0].time];
 
     client.query(sql, vars, (err, res) => {
         if (err) {
