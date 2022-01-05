da8b60e43d2dc6af8b62328922623a068b411bbb
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2022-01-05 20:39:07 +0200

    unit test post petRegister- pet is exist

--- Back/controller/__test__/pets.test.js
+++ Back/controller/__test__/pets.test.js
@@ -18,3 +18,21 @@ const app = require('../../index')
 
 //     });
 // });
+
+describe("POST /pets/petRegister", () => {
+    it("It should respond 'pet is exist'", async () => {
+        const newPet = await request(app).post("/pets/petRegister").send({
+            phone_number: '0528287761',
+            name: 'bell',
+            breed: 'milow',
+            chip_number: '',
+            birthday: '11/02/2009',
+            gender: 'famle',
+            type: 'dog'
+        });
+        expect(newPet.body.message).toBe("pet is exist");
+        expect(newPet.statusCode).toBe(400);
+
+    });
+});
+
