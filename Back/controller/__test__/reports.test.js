6bdc9bfa861fb81c9b8ae185015d24b248035751
EfratKadosh <95250966+EfratKadosh@users.noreply.github.com>
2022-01-05 20:17:02 +0200

    unit test reports-uploadreport-return 400 error

--- Back/controller/__test__/reports.test.js
+++ Back/controller/__test__/reports.test.js
@@ -46,3 +46,16 @@ describe("POST /reports/uploadReport", () => {
 
     });
 });
+
+describe("POST /reports/uploadReport", () => {
+    it("It should respond 'error'", async () => {
+        const User = await request(app).post("/reports/uploadReport").send({
+            phone_number: '0528287761',
+            name: 'bell',
+            appointment_type: 'lab',
+        });
+        expect(User.body.message).toBe("error");
+        expect(User.statusCode).toBe(400);
+
+    });
+});
