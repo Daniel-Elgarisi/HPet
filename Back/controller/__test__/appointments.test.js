c1ffea56a9401971af1c18dea91719c6e167279f
EfratKadosh <95250966+EfratKadosh@users.noreply.github.com>
2022-01-05 20:06:21 +0200

    unit test post appointment make  - user not found

--- Back/controller/__test__/appointments.test.js
+++ Back/controller/__test__/appointments.test.js
@@ -16,3 +16,18 @@ const app = require('../../index')
 
 //     });
 // });
+
+describe("POST /appointments/make", () => {
+    it("It should respond 'user is not found'", async () => {
+        const User = await request(app).post("/appointments/make").send({
+            phone_number: '0528287768',
+            name: 'bell',
+            appointment_type: 'vaccine',
+            time: '2022-12-26 19:30'
+
+        });
+        expect(User.body.message).toBe("user is not found");
+        expect(User.statusCode).toBe(400);
+
+    });
+});
