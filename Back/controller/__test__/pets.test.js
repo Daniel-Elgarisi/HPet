3a8d65a1f8c5d9faa6bf4573acdc64fa9d629772
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2022-01-05 20:38:01 +0200

    unit test post for petRegister- New pet created...

---
+++ Back/controller/__test__/pets.test.js
@@ -0,0 +1,20 @@
+const client = require('../../db/db')
+const request = require('supertest');
+const app = require('../../index')
+
+// describe("POST /pets/petRegister", () => {
+//     it("It should respond 'New pet created successfully'", async () => {
+//         const newPet = await request(app).post("/pets/petRegister").send({
+//             phone_number: '0528287761',
+//             name: 'rup',
+//             breed: 'milow',
+//             chip_number: '',
+//             birthday: '11/02/2009',
+//             gender: 'famle',
+//             type: 'dog'
+//         });
+//         expect(newPet.body.message).toBe("New pet created successfully");
+//         expect(newPet.statusCode).toBe(200);
+
+//     });
+// });
