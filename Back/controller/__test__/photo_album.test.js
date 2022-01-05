41fc9a0fb7dc9c3782e8786ca70cdfd1e133983f
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2022-01-05 21:03:55 +0200

    unitest get for watchAlbum- code 200

--- Back/controller/__test__/photo_album.test.js
+++ Back/controller/__test__/photo_album.test.js
@@ -1,3 +1,12 @@
 const client = require('../../db/db')
 const request = require('supertest');
 const app = require('../../index')
+
+
+describe("GET /photo/watchAlbum", () => {
+    it("It's Ok!", async () => {
+        const response = await request(app).get("/photo/watchAlbum/0528287761/bell");
+        expect(response.body[0]).toHaveProperty("url");
+        expect(response.statusCode).toBe(200);
+    });
+});
