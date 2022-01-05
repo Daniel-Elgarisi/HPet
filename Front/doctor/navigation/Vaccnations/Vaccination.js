7bc96fbd66a81abbcf87023e4b52d37d3159856d
BeckyU96 <beckyu96@gmail.com>
2022-01-05 19:21:47 +0200

    added doctor vaccination files - Front

--- Front/doctor/navigation/Vaccnations/Vaccination.js
+++ Front/doctor/navigation/Vaccnations/Vaccination.js
@@ -1,11 +1,12 @@
-let ownerId
-let petId
-
 async function updateVaccein() {
+    let phone = document.getElementById("Phone").value;
+    let pname = document.getElementById("Pname").value;
     let vaccein = document.getElementById("Vacceins").value;
+    let ip = sessionStorage.getItem("ip")
+
     //fetch
     //call for POST to the url:
-    let response = await fetch('http://localhost:5000/pets/updateVaccein', {
+    let response = await fetch(`http://${ip}:5000/vaccines/updateVaccein`, {
         //post
         method: 'POST',
         headers: {
@@ -13,61 +14,13 @@ async function updateVaccein() {
         },
         //this is the stuff we refer to as: req.body in the backend!!!!!
         body: JSON.stringify({
-            petId: petId.data,
-            vaccein: vaccein
+            phone_number: phone,
+            petname: pname,
+            vaccine_type: vaccein
         })
     })
     //get data from backend response as json!
     let body = await response.json()
+
     alert(body.message)
-}
-
-async function findPetIdByName() {
-    let pname = document.getElementById("Pname").value;
-
-    //fetch
-    //call for POST to the url:
-    let response = await fetch('http://localhost:5000/pets/findPetIdByName', {
-        //post
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        //this is the stuff we refer to as: req.body in the backend!!!!!
-        body: JSON.stringify({
-            pname: pname,
-            ownerId: ownerId.data
-        })
-    })
-    //get data from backend response as json!
-    petId = await response.json()
-}
-
-
-async function findOwner() {
-    let phone = document.getElementById("Phone").value;
-
-    //fetch
-    //call for POST to the url:
-    let response = await fetch('http://localhost:5000/user/getUserByPhone', {
-        //post
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        //this is the stuff we refer to as: req.body in the backend!!!!!
-        body: JSON.stringify({
-            phone: phone
-        })
-    })
-    //get data from backend response as json!
-    ownerId = await response.json()
-    let x = document.getElementById("Pname");
-    if (ownerId.data) {
-        x.removeAttribute("disabled");
-    }
-    else{
-        x.setAttribute("disabled", "");
-    }
-    console.log(ownerId);
 }
