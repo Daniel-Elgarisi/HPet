50bcc83c499762ada3d575717aa9b224de12a542
yovelEfraim <95250655+yovelEfraim@users.noreply.github.com>
2022-01-05 20:55:19 +0200

    unitest post for register- New user created successfully

--- Back/controller/__test__/user.test.js
+++ Back/controller/__test__/user.test.js
@@ -1,3 +1,20 @@
 const client = require('../../db/db')
 const request = require('supertest');
 const app = require('../../index')
+
+// describe("POST /user/register", () => {
+//     it("It should respond 'New user created successfully'", async () => {
+//         const newUser = await request(app).post("/user/register").send({
+//             user_name: 'ddi',
+//             password: '123',
+//             first_name: 'dori',
+//             last_name: 'fourer',
+//             email: 'ddi@gmail.com',
+//             phone_number: '0648929226',
+//             role: 1
+//         });
+//         expect(newUser.body.message).toBe("New user created successfully");
+//         expect(newUser.statusCode).toBe(200);
+
+//     });
+// });
