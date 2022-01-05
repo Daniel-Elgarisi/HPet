48d6e9eb8461ff0334f111ecb0dd1f8a1f44870c
EfratKadosh <95250966+EfratKadosh@users.noreply.github.com>
2022-01-05 20:14:41 +0200

    unit test reports-uploadreport-return report successfully

---
+++ Back/controller/__test__/reports.test.js
@@ -0,0 +1,18 @@
+const client = require('../../db/db')
+const request = require('supertest');
+const app = require('../../index')
+
+
+describe("POST /reports/uploadReport", () => {
+    it("It should respond 'report is created successfully'", async () => {
+        const User = await request(app).post("/reports/uploadReport").send({
+            phone_number: '0528287761',
+            name: 'bell',
+            appointment_type: 'lab',
+            report: 'test1',
+        });
+        expect(User.body.message).toBe("report is created successfully");
+        expect(User.statusCode).toBe(200);
+
+    });
+});
